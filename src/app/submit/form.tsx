c955f1b5a3f99256a94da5e76156da9694b5ac5e
aelew <118043566+aelew@users.noreply.github.com>
2024-02-20 17:57:38 -0800

    add submit definition form page

---
+++ src/app/submit/form.tsx
@@ -0,0 +1,115 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+
+const formSchema = z.object({
+  term: z
+    .string()
+    .min(1)
+    .max(64, { message: 'Term must be between 1 and 64 characters.' }),
+  definition: z
+    .string()
+    .min(1)
+    .max(512, { message: 'Definition must be between 1 and 512 characters.' }),
+  example: z
+    .string()
+    .min(1)
+    .max(512, { message: 'Example must be between 1 and 512 characters.' })
+});
+
+export function SubmitDefinitionForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      term: '',
+      definition: '',
+      example: ''
+    }
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-4"
+      >
+        <FormField
+          name="term"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Term</FormLabel>
+              <FormControl>
+                <Input placeholder="developer" {...field} />
+              </FormControl>
+              <FormDescription>
+                The word or phrase you want to define.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="definition"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Definition</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="a person or company who builds something such as an idea, a design, or a product"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                Your definition should be clear to everyone, no inside jokes!
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="example"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Example</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Bob the Builder decided to stop pursuing a developer position because the job market was too competitive."
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                How would you use this term in a sentence?
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button>Submit my definition</Button>
+      </form>
+    </Form>
+  );
+}
