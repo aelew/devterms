e41bf333656cfcad27ec64d488ed4137bde6ed9b
aelew <118043566+aelew@users.noreply.github.com>
2024-02-21 15:21:43 -0800

    exclude wotd from home feed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,28 @@
-import { eq, sql } from 'drizzle-orm';
+import { desc, eq, sql } from 'drizzle-orm';
 import type { Metadata } from 'next';
 import { unstable_cache } from 'next/cache';
 
 import { AsideCard } from '@/components/aside-card';
 import { DefinitionCard } from '@/components/definition-card';
-import { WordOfTheDay } from '@/components/word-of-the-day';
 import { db } from '@/server/db';
-import { definitions } from '@/server/db/schema';
+import { definitions, wotds } from '@/server/db/schema';
 
 export const metadata: Metadata = { title: 'The Developer Dictionary' };
+
+const getWordOfTheDay = unstable_cache(
+  () =>
+    db.query.wotds.findFirst({
+      orderBy: desc(wotds.createdAt),
+      with: {
+        definition: {
+          with: {
+            user: true
+          }
+        }
+      }
+    }),
+  ['wotd']
+);
 
 const getHomeFeed = unstable_cache(
   () =>
@@ -22,14 +36,25 @@ const getHomeFeed = unstable_cache(
 );
 
 export default async function HomePage() {
-  const homeFeed = await getHomeFeed();
+  const [wotd, homeFeed] = await Promise.all([
+    getWordOfTheDay(),
+    getHomeFeed()
+  ]);
   return (
     <div className="flex flex-col-reverse gap-4 md:flex-row">
       <div className="flex flex-1 flex-col gap-4">
-        <WordOfTheDay />
-        {homeFeed.map((definition) => (
-          <DefinitionCard key={definition.id} definition={definition} />
-        ))}
+        {wotd && (
+          <DefinitionCard
+            definition={wotd.definition}
+            badges={['Word of the day']}
+            className="border-muted-foreground shadow-lg"
+          />
+        )}
+        {homeFeed
+          .filter((def) => !wotd || def.id !== wotd.id)
+          .map((def) => (
+            <DefinitionCard key={def.id} definition={def} />
+          ))}
       </div>
       <AsideCard />
     </div>
