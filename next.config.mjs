6db69a51e0b1fe8fb1438ef7f338de4032c6fb4f
aelew <118043566+aelew@users.noreply.github.com>
2024-02-21 00:47:05 -0800

    disable eslint/ts build error checking

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const jiti = createJiti(new URL(import.meta.url).pathname);
 jiti('./src/env');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }
+};
 
 export default nextConfig;
