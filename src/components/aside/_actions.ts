1f6b741d204d992a8e46e9453c386dde1a66e352
aelew <118043566+aelew@users.noreply.github.com>
2024-02-23 01:50:41 -0800

    fix "I'm feeling lucky" redirect slug

--- src/components/aside/_actions.ts
+++ src/components/aside/_actions.ts
@@ -3,11 +3,12 @@
 import { redirect } from 'next/navigation';
 
 import { getRandomDefinition } from '@/lib/definitions';
+import { termToSlug } from '@/lib/utils';
 
 export async function showRandomDefinition() {
   const definition = await getRandomDefinition();
   if (!definition) {
     throw new Error('No definitions available');
   }
-  redirect(`/define/${definition.term}`);
+  redirect(`/define/${termToSlug(definition.term)}`);
 }
