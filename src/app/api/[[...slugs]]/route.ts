448ded8a2987e9fd6efeabbb0b76d3d0053c411f
aelew <118043566+aelew@users.noreply.github.com>
2024-02-22 19:57:05 -0800

    automatically tweet on WOTD cron job execution

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -5,6 +5,7 @@ import { Elysia, t } from 'elysia';
 import MeiliSearch from 'meilisearch';
 import { revalidateTag } from 'next/cache';
 import { cookies } from 'next/headers';
+import { TwitterApi } from 'twitter-api-v2';
 
 import { env } from '@/env';
 import { github, lucia } from '@/lib/auth';
@@ -16,23 +17,20 @@ import { db } from '@/server/db';
 import { definitions, users, wotds } from '@/server/db/schema';
 import type { GitHubEmailsResponse, GitHubUserResponse } from '@/types';
 
+const TWITTER_CALLBACK_URL = `${env.NEXT_PUBLIC_BASE_URL}/api/callback/twitter`;
+
 const app = new Elysia({ prefix: '/api' })
   .use(
     swagger({
       path: '/docs',
+      exclude: /internal|callback|cron|docs/,
       documentation: {
         info: {
           version: '1.0.0',
           title: `${APP_NAME} API`,
           description: APP_DESCRIPTION
         }
-      },
-      exclude: [
-        '/api/callback/github',
-        '/api/cron/meilisearch',
-        '/api/cron/wotd',
-        '/api/docs'
-      ]
+      }
     })
   )
   .onError(({ code }) => {
@@ -43,92 +41,169 @@ const app = new Elysia({ prefix: '/api' })
   .onTransform(({ set }) => {
     set.headers['content-type'] = 'application/json';
   })
-  .get(
-    '/callback/github',
-    async ({ query, cookie, set }) => {
-      if (query.state !== cookie.oauth_state.get()) {
-        set.status = 400;
-        return { error: 'invalid_parameters' };
-      }
-
-      try {
-        const tokens = await github.validateAuthorizationCode(query.code);
-        const githubUserResponse = await fetch('https://api.github.com/user', {
-          headers: { Authorization: `Bearer ${tokens.accessToken}` }
-        });
-        const githubUser: GitHubUserResponse = await githubUserResponse.json();
-
-        const existingUser = await db.query.users.findFirst({
-          where: eq(users.githubId, githubUser.id)
-        });
-
-        if (existingUser) {
-          const session = await lucia.createSession(existingUser.id, {});
-          const sessionCookie = lucia.createSessionCookie(session.id);
-          cookies().set(
-            sessionCookie.name,
-            sessionCookie.value,
-            sessionCookie.attributes
-          );
-          set.redirect = '/';
-          return;
-        }
-
-        let userEmail: string | null = githubUser.email;
-
-        // Special case for GitHub accounts with private emails
-        if (!userEmail) {
-          const githubEmailsResponse = await fetch(
-            'https://api.github.com/user/emails',
-            { headers: { Authorization: `Bearer ${tokens.accessToken}` } }
-          );
-          const githubEmails: GitHubEmailsResponse =
-            await githubEmailsResponse.json();
-
-          userEmail =
-            githubEmails.find((email) => email.primary)?.email ??
-            githubEmails.find((email) => email.verified)?.email ??
-            null;
-
-          if (!userEmail) {
-            set.status = 400;
-            return { error: 'missing_github_email' };
-          }
-        }
-
-        const userId = generateId('user');
-
-        await db.insert(users).values({
-          id: userId,
-          email: userEmail,
-          name: githubUser.login,
-          githubId: githubUser.id,
-          avatar: githubUser.avatar_url
-        });
-
-        const session = await lucia.createSession(userId, {});
-        const sessionCookie = lucia.createSessionCookie(session.id);
-        cookies().set(
-          sessionCookie.name,
-          sessionCookie.value,
-          sessionCookie.attributes
-        );
-
-        set.redirect = '/';
-      } catch (err) {
-        if (err instanceof OAuth2RequestError) {
-          set.status = 400;
-          return { error: 'invalid_code' };
-        }
-        console.error('GitHub OAuth2 error:', err);
-        set.status = 500;
-        return { error: 'internal_server_error' };
-      }
-    },
-    {
-      query: t.Object({ code: t.String(), state: t.String() }),
-      cookie: t.Object({ oauth_state: t.String() })
+  .get('/internal-twitter-login', async ({ set }) => {
+    if (!env.TWITTER_CONSUMER_KEY || !env.TWITTER_CONSUMER_SECRET) {
+      set.status = 500;
+      return { error: 'twitter_consumer_keys_missing' };
     }
+
+    const twitter = new TwitterApi({
+      appKey: env.TWITTER_CONSUMER_KEY,
+      appSecret: env.TWITTER_CONSUMER_SECRET
+    });
+
+    const authLink = await twitter.generateAuthLink(TWITTER_CALLBACK_URL, {
+      linkMode: 'authorize'
+    });
+
+    const options = {
+      secure: env.NODE_ENV === 'production',
+      maxAge: 60 * 10,
+      httpOnly: true,
+      sameSite: 'lax',
+      path: '/'
+    } as const;
+
+    set.cookie = {
+      internal_twitter_oauth_token: {
+        ...options,
+        value: authLink.oauth_token
+      },
+      internal_twitter_oauth_token_secret: {
+        ...options,
+        value: authLink.oauth_token_secret
+      }
+    };
+
+    set.redirect = authLink.url;
+  })
+  .group('/callback', (app) =>
+    app
+      .get(
+        '/twitter',
+        async ({ query, cookie, set }) => {
+          if (!env.TWITTER_CONSUMER_KEY || !env.TWITTER_CONSUMER_SECRET) {
+            set.status = 500;
+            return { error: 'twitter_consumer_keys_missing' };
+          }
+
+          const twitter = new TwitterApi({
+            appKey: env.TWITTER_CONSUMER_KEY,
+            appSecret: env.TWITTER_CONSUMER_SECRET,
+            accessToken: cookie.internal_twitter_oauth_token.get(),
+            accessSecret: cookie.internal_twitter_oauth_token_secret.get()
+          });
+          try {
+            const { accessToken, accessSecret } = await twitter.login(
+              query.oauth_verifier
+            );
+            return { accessToken, accessSecret };
+          } catch (err) {
+            set.status = 500;
+            console.error('Twitter OAuth error:', err);
+            return { error: 'internal_server_error' };
+          }
+        },
+        {
+          query: t.Object({
+            oauth_token: t.String(),
+            oauth_verifier: t.String()
+          }),
+          cookie: t.Object({
+            internal_twitter_oauth_token: t.String(),
+            internal_twitter_oauth_token_secret: t.String()
+          })
+        }
+      )
+      .get(
+        '/github',
+        async ({ query, cookie, set }) => {
+          if (query.state !== cookie.oauth_state.get()) {
+            set.status = 400;
+            return { error: 'invalid_parameters' };
+          }
+
+          try {
+            const tokens = await github.validateAuthorizationCode(query.code);
+            const githubUserResponse = await fetch(
+              'https://api.github.com/user',
+              { headers: { Authorization: `Bearer ${tokens.accessToken}` } }
+            );
+            const githubUser: GitHubUserResponse =
+              await githubUserResponse.json();
+
+            const existingUser = await db.query.users.findFirst({
+              where: eq(users.githubId, githubUser.id)
+            });
+
+            if (existingUser) {
+              const session = await lucia.createSession(existingUser.id, {});
+              const sessionCookie = lucia.createSessionCookie(session.id);
+              cookies().set(
+                sessionCookie.name,
+                sessionCookie.value,
+                sessionCookie.attributes
+              );
+              set.redirect = '/';
+              return;
+            }
+
+            let userEmail: string | null = githubUser.email;
+
+            // Special case for GitHub accounts with private emails
+            if (!userEmail) {
+              const githubEmailsResponse = await fetch(
+                'https://api.github.com/user/emails',
+                { headers: { Authorization: `Bearer ${tokens.accessToken}` } }
+              );
+              const githubEmails: GitHubEmailsResponse =
+                await githubEmailsResponse.json();
+
+              userEmail =
+                githubEmails.find((email) => email.primary)?.email ??
+                githubEmails.find((email) => email.verified)?.email ??
+                null;
+
+              if (!userEmail) {
+                set.status = 400;
+                return { error: 'github_email_missing' };
+              }
+            }
+
+            const userId = generateId('user');
+
+            await db.insert(users).values({
+              id: userId,
+              email: userEmail,
+              name: githubUser.login,
+              githubId: githubUser.id,
+              avatar: githubUser.avatar_url
+            });
+
+            const session = await lucia.createSession(userId, {});
+            const sessionCookie = lucia.createSessionCookie(session.id);
+            cookies().set(
+              sessionCookie.name,
+              sessionCookie.value,
+              sessionCookie.attributes
+            );
+
+            set.redirect = '/';
+          } catch (err) {
+            if (err instanceof OAuth2RequestError) {
+              set.status = 400;
+              return { error: 'invalid_code' };
+            }
+            set.status = 500;
+            console.error('GitHub OAuth2 error:', err);
+            return { error: 'internal_server_error' };
+          }
+        },
+        {
+          query: t.Object({ code: t.String(), state: t.String() }),
+          cookie: t.Object({ oauth_state: t.String() })
+        }
+      )
   )
   .group('/cron', (app) =>
     app.guard({ headers: t.Object({ authorization: t.String() }) }, (app) =>
@@ -166,8 +241,36 @@ const app = new Elysia({ prefix: '/api' })
             set.status = 500;
             return { error: 'no_definitions_available' };
           }
+
           await db.insert(wotds).values({ definitionId: definition.id });
           revalidateTag('wotd');
+
+          // Post on X (Twitter)
+          if (
+            env.TWITTER_CONSUMER_KEY &&
+            env.TWITTER_CONSUMER_SECRET &&
+            env.TWITTER_ACCESS_TOKEN &&
+            env.TWITTER_ACCESS_TOKEN_SECRET
+          ) {
+            const twitter = new TwitterApi({
+              appKey: env.TWITTER_CONSUMER_KEY,
+              appSecret: env.TWITTER_CONSUMER_SECRET,
+              accessToken: env.TWITTER_ACCESS_TOKEN,
+              accessSecret: env.TWITTER_ACCESS_TOKEN_SECRET
+            });
+            try {
+              await twitter.v2.tweet(
+                `Today's #developer word of the day is ${definition.term}! 💡\n\n` +
+                  `${definition.term}: ${definition.definition}\n\n` +
+                  `🔗 ${env.NEXT_PUBLIC_BASE_URL}/define/${termToSlug(definition.term)}`
+              );
+            } catch (err) {
+              set.status = 500;
+              console.error('Twitter error:', err);
+              return { error: 'internal_server_error' };
+            }
+          }
+
           return { success: true };
         })
     )
