d96c6e46ecafba32f10f961145da6453408c43f6
aelew <118043566+aelew@users.noreply.github.com>
2024-02-23 23:32:50 -0800

    use a sitemap index

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,7 +4,7 @@ import { env } from '@/env';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    sitemap: env.NEXT_PUBLIC_BASE_URL + '/sitemap.xml',
+    sitemap: env.NEXT_PUBLIC_BASE_URL + '/sitemap-index.xml',
     host: env.NEXT_PUBLIC_BASE_URL,
     rules: {
       userAgent: '*',
