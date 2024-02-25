85f4c4ea0594b395a1f1fa040fb409e3c0b664f3
aelew <118043566+aelew@users.noreply.github.com>
2024-02-24 23:41:39 -0800

    add /about to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,10 +3,12 @@ import type { MetadataRoute } from 'next';
 import { env } from '@/env';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return ['/', '/browse', '/submit', '/api/docs', '/login'].map((path) => ({
-    url: env.NEXT_PUBLIC_BASE_URL + path,
-    priority: path === '/' ? 1 : 0.9,
-    changeFrequency: 'daily',
-    lastModified: new Date()
-  }));
+  return ['/', '/browse', '/submit', '/api/docs', '/login', '/about'].map(
+    (path) => ({
+      url: env.NEXT_PUBLIC_BASE_URL + path,
+      priority: path === '/' ? 1 : 0.9,
+      changeFrequency: 'daily',
+      lastModified: new Date()
+    })
+  );
 }
