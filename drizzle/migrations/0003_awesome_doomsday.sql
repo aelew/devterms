2381abdb15c3ed57e4468edcd282e81809a3dafb
aelew <118043566+aelew@users.noreply.github.com>
2024-02-25 20:46:50 -0800

    drop all drizzle migrations due to conflicts

--- drizzle/migrations/0003_awesome_doomsday.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `users` MODIFY COLUMN `github_id` int unsigned NOT NULL;
--- drizzle/migrations/0007_flowery_princess_powerful.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `users` MODIFY COLUMN `role` enum('user','bot','moderator','owner') NOT NULL DEFAULT 'user';
