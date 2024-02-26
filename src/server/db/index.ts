3f0008e1f9678cdbaed099b3e8395fdb5c0bcddc
aelew <118043566+aelew@users.noreply.github.com>
2024-02-26 13:39:05 -0800

    remove db query logging in dev

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -6,7 +6,4 @@ import * as schema from './schema';
 
 const client = new Client({ url: env.DATABASE_URL });
 
-export const db = drizzle(client, {
-  schema,
-  logger: env.NODE_ENV === 'development'
-});
+export const db = drizzle(client, { schema });
