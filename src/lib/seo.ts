4f772c420e45d38a6c205c102ce9f862861df503
aelew <118043566+aelew@users.noreply.github.com>
2024-02-27 11:34:09 -0800

    add og image

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -26,13 +26,22 @@ export const baseMetadata = {
     locale: 'en_US',
     title: APP_NAME,
     siteName: APP_NAME,
-    description: APP_DESCRIPTION
+    description: APP_DESCRIPTION,
+    images: [
+      {
+        url: `${env.NEXT_PUBLIC_BASE_URL}/media/og.jpg`,
+        alt: APP_NAME,
+        width: 1200,
+        height: 630
+      }
+    ]
   },
   twitter: {
     title: APP_NAME,
     creator: '@aelew_',
     card: 'summary_large_image',
-    description: APP_DESCRIPTION
+    description: APP_DESCRIPTION,
+    images: [`${env.NEXT_PUBLIC_BASE_URL}/media/og.jpg`]
   }
 } satisfies Metadata;
 
