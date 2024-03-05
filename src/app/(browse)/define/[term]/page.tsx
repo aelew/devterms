be1af114752a4333ff0a493963f43a81ec856e7f
Andre Lew <me@aelew.com>
2024-03-05 11:45:40 -0800

    add dynamic OG images (#38)
    * add dynamic OG images
    
    * add OG secret env variable

--- src/app/(browse)/define/[term]/page.tsx
+++ src/app/(browse)/define/[term]/page.tsx
@@ -1,6 +1,8 @@
+import { createHmac } from 'crypto';
 import { Suspense } from 'react';
 
 import { DefinitionCardSkeleton } from '@/components/definition-card/skeleton';
+import { env } from '@/env';
 import { getPageMetadata } from '@/lib/seo';
 import { slugToTerm } from '@/lib/utils';
 import { DefineResultCards, getDefinitions } from './_components/result-cards';
@@ -17,9 +19,28 @@ export async function generateMetadata({ params }: DefinitionPageProps) {
   if (!results.length) {
     return getPageMetadata({ title: `What is ${term}?` });
   }
+  const firstResult = results[0]!;
+
+  const hmac = createHmac('sha256', env.OG_HMAC_SECRET);
+  hmac.update(JSON.stringify({ slug: params.term }));
+
+  const ogToken = hmac.digest('hex');
+  const ogUrl = `${env.NEXT_PUBLIC_BASE_URL}/api/og/${params.term}?t=${ogToken}`;
+
   return getPageMetadata({
-    title: `What is ${results[0]!.term}?`,
-    description: results[0]!.definition
+    title: `What is ${firstResult.term}?`,
+    description: firstResult.definition,
+    twitter: { images: [ogUrl] },
+    openGraph: {
+      images: [
+        {
+          url: ogUrl,
+          width: 1200,
+          height: 630,
+          alt: firstResult.term
+        }
+      ]
+    }
   });
 }
 
